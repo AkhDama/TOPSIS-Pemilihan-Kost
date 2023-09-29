e44a876ba38c2cd1c2286e2397fb38f9e97f975a
akh_dama <damapandan@gmail.com>
2023-09-30 01:00:55 +0700

    first commit

---
+++ Database TOPSIS/topsis_kost.sql
@@ -0,0 +1,153 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 19 Nov 2021 pada 14.54
+-- Versi server: 10.1.34-MariaDB
+-- Versi PHP: 5.6.37
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `topsis_kost`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `idadmin` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `level` varchar(10) NOT NULL,
+  `namalengkap` varchar(30) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `admin`
+--
+
+INSERT INTO `admin` (`idadmin`, `username`, `password`, `level`, `namalengkap`) VALUES
+(1, 'admin', 'ADMIN', 'ADMIN', 'ADMIN'),
+(2, 'dama', 'dama', 'USER', 'dama putra sarpanda');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `alternatif`
+--
+
+CREATE TABLE `alternatif` (
+  `id_alternatif` varchar(5) NOT NULL,
+  `nm_alternatif` varchar(35) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kriteria`
+--
+
+CREATE TABLE `kriteria` (
+  `id_kriteria` varchar(5) NOT NULL,
+  `nama_kriteria` varchar(45) NOT NULL,
+  `bobot` double NOT NULL,
+  `poin1` double NOT NULL,
+  `poin2` double NOT NULL,
+  `poin3` double NOT NULL,
+  `poin4` double NOT NULL,
+  `poin5` double NOT NULL,
+  `sifat` varchar(15) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `nilai_matrik`
+--
+
+CREATE TABLE `nilai_matrik` (
+  `id_matrik` int(7) NOT NULL,
+  `id_alternatif` varchar(7) NOT NULL,
+  `id_kriteria` varchar(7) NOT NULL,
+  `nilai` double NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `nilai_preferensi`
+--
+
+CREATE TABLE `nilai_preferensi` (
+  `nm_alternatif` varchar(35) NOT NULL,
+  `nilai` double NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`idadmin`);
+
+--
+-- Indeks untuk tabel `alternatif`
+--
+ALTER TABLE `alternatif`
+  ADD PRIMARY KEY (`id_alternatif`);
+
+--
+-- Indeks untuk tabel `kriteria`
+--
+ALTER TABLE `kriteria`
+  ADD PRIMARY KEY (`id_kriteria`);
+
+--
+-- Indeks untuk tabel `nilai_matrik`
+--
+ALTER TABLE `nilai_matrik`
+  ADD PRIMARY KEY (`id_matrik`);
+
+--
+-- Indeks untuk tabel `nilai_preferensi`
+--
+ALTER TABLE `nilai_preferensi`
+  ADD PRIMARY KEY (`nm_alternatif`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `idadmin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `nilai_matrik`
+--
+ALTER TABLE `nilai_matrik`
+  MODIFY `id_matrik` int(7) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
